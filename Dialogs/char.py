4a0b1b5e7ae2a8fc2270d32c3ad7f1f9566287d2
Xaooc <101577885+C2Dimplementation@users.noreply.github.com>
2022-11-09 23:59:59 +0300

    реализованы команды

---
+++ Dialogs/char.py
@@ -0,0 +1,30 @@
+from aiogram.fsm.context import FSMContext
+from aiogram.types import Message
+from aiogram.filters import Command
+from aiogram import Router
+
+from database import UserData
+
+
+router = Router()
+
+
+@router.message(Command("char"))
+async def char(message: Message, state: FSMContext):
+    user = UserData(tg_id=message.from_user.id)
+    #проверяем есть ли активный персонаж
+    if user.is_user_inactive_char():
+        await message.answer('У тебя ещё нет активного персонажа. Создать его ты можешь с помощью команды /new')
+    else:
+        char = await user.get_char_name()
+        name = char.get('name')
+        con = char.get('con')
+        dex = char.get('dex')
+        mnd = char.get('mnd')
+        gold = char.get('gold')
+        await message.answer(f'*Имя персонажа:* {name}\n'
+                             f'*Телосложение:* {con}  *Ловкость:* {dex}  *Мудрость:* {mnd}\n'
+                             f'*Золото:* {gold}', parse_mode="MarkdownV2")
+
+
+
---
+++ Dialogs/info.py
@@ -0,0 +1,19 @@
+from aiogram.fsm.context import FSMContext
+from aiogram.types import Message
+from aiogram.filters import Command
+from aiogram import Router
+
+router = Router()
+
+
+@router.message(Command("info"))
+async def start(message: Message, state: FSMContext):
+    await message.answer('Ты попал в бесконечные подземелья. '
+                         'Здесь твой персонаж будет добывать золото, пока ты занимаешься своими делами'
+                         'Иногда от тебя потребуется выбрать как поступить персонажу - '
+                         'от твоего выбора будут зависеть его действия и шансы на успех\n\n'
+                         'Позже планирую добавить в игру режим группы, где на группу будет создаваться '
+                         'отдельный персонаж и выбор его действий будет происходить общим голосованием.\n\n'
+                         'Предложения и пожелания можете писать @Xaoac41')
+
+
