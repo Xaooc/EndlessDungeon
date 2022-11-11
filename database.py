d662bd5470ac6293d9553364dec91064fbcb1fd4
Xaooc <101577885+C2Dimplementation@users.noreply.github.com>
2022-11-11 03:57:54 +0300

    Добавлены функции для работы со статусом юзера, изменением хп и золота персонажа

--- database.py
+++ database.py
@@ -10,7 +10,8 @@ chars = Table('characters', meta, autoload=True)
 users = Table('users', meta, autoload=True)
 session = Session(bind=engine)
 
-### инициализируем классы таблиц
+
+# инициализируем классы таблиц
 class Chars:
     def __init__(self, name: str, tg_id: int, con: int, dex: int, mnd: int,
                  gold: int, hp: int = 0, lvl: int = 0, place: int = 0, is_dead: bool = False):
@@ -26,25 +27,23 @@ class Chars:
         self.is_dead = is_dead
 
 
-class Place:
-    def __init__(self, name: str, desc: str, event: str):
-        self.name = name
-        self.desc = desc
-        self.event = event
-
-
 class Users:
-    def __init__(self, tg_id: int, id_char: int):
+    def __init__(self, tg_id: int, id_char: int, status: int = 0):
+        self.status = status
         self.tg_id = tg_id
         self.id_char = id_char
 
-#маппим их с таблицей бд
+
+# мапим их с таблицей бд
 mapper(Users, users)
 mapper(Chars, chars)
+
+
 ###
 
 class UserData:
     def __init__(self, tg_id: int):
+        self.max_hp = 0
         self.tg_id = tg_id
         self.id_char = 0
         self.name = ''
@@ -59,7 +58,7 @@ class UserData:
         """
         return session.query(Users).where(Users.tg_id == self.tg_id).scalar()
 
-    def user_char_upd(self):
+    async def user_char_upd(self) -> None:
         """
         Обновление активного персонажа у юзера
         """
@@ -68,7 +67,7 @@ class UserData:
         session.add(user)
         session.commit()
 
-    async def create_char(self):
+    async def create_char(self) -> None:
         """
         Создание нового персонажа и запись его юзеру
         """
@@ -76,7 +75,7 @@ class UserData:
         if not self.is_user_created():
             session.add(user)
         else:
-            self.user_char_upd()
+            await self.user_char_upd()
         session.commit()
 
     def is_user_inactive_char(self) -> bool:
@@ -85,7 +84,7 @@ class UserData:
         """
         return session.query(Users).where(Users.tg_id == self.tg_id).where(Users.id_char == 0).scalar()
 
-    async def new_char(self, name: str, con: int, dex: int, mnd: int, gold: int):
+    async def new_char(self, name: str, con: int, dex: int, mnd: int, gold: int) -> None:
         """
         Создание и запись нового персонажа в бд
         """
@@ -94,8 +93,9 @@ class UserData:
         self.dex = dex
         self.mnd = mnd
         self.gold = gold
+        self.max_hp = 10 + (self.con - 10) // 2
 
-        new = Chars(self.name, self.tg_id, self.con, self.dex, self.mnd, self.gold)
+        new = Chars(self.name, self.tg_id, self.con, self.dex, self.mnd, self.gold, hp=self.max_hp)
         session.add(new)
         session.commit()
         session.refresh(new)
@@ -109,3 +109,45 @@ class UserData:
         self.id_char = user.id_char
         char = session.get(Chars, self.id_char)
         return {'name': char.name, 'con': char.con, 'dex': char.dex, 'mnd': char.mnd, 'gold': char.gold}
+
+    async def gold_mod(self, gold: int) -> None:
+        """
+        Изменение кол-ва золота
+        """
+        user = session.query(Users).where(Users.tg_id == self.tg_id).one()
+        self.id_char = user.id_char
+        char = session.get(Chars, self.id_char)
+        char.gold += gold if (char.gold + gold) >= 0 else 0
+        session.add(char)
+        session.commit()
+
+    async def hp_mod(self, hp: int) -> None:
+        """
+        Изменение кол-ва здоровья
+        """
+        user = session.query(Users).where(Users.tg_id == self.tg_id).one()
+        self.id_char = user.id_char
+        char = session.get(Chars, self.id_char)
+        char.hp += hp if (char.hp + hp) >= 0 else 0
+        if char.hp == 0:
+            pass
+        if char.hp > self.max_hp:
+            char.hp = self.max_hp
+        session.add(char)
+        session.commit()
+
+    async def update_stats(self, status: int) -> None:
+        """
+        Обновление статуса пользователя. Нужно для защиты от повторного вызова команды
+        """
+        user = session.query(Users).where(Users.tg_id == self.tg_id).one()
+        user.status = status
+        session.add(user)
+        session.commit()
+
+    async def get_user_status(self) -> int:
+        """
+        Получить статус пользователя
+        """
+        user = session.query(Users).where(Users.tg_id == self.tg_id).one()
+        return user.status
