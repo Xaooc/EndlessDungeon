837e5b54243a53bcbeea0c983f72ee1ff545426a
Xaooc <101577885+C2Dimplementation@users.noreply.github.com>
2022-11-13 05:30:39 +0300

    Добавлена проверка на даты пересоздания персонажа

--- Dialogs/new_char.py
+++ Dialogs/new_char.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+
 from aiogram.fsm.context import FSMContext
 from aiogram.types import Message
 from aiogram.filters import Command
@@ -16,11 +18,15 @@ async def new(message: Message, state: FSMContext):
     #записываем экземпляр, чтобы передавать между статусами
     user = UserData(tg_id=message.from_user.id)
     await state.update_data(user=user)
+    date = await user.get_char_name()
+    date = date.get('date')
     if not user.is_user_created():
         await user.create_char()
     if user.is_user_inactive_char():
-        await message.answer('Как зовут твоего персонажа?')
+        await message.answer('Как зовут твоего персонажа?', reply_markup=bt.ReplyKeyboardRemove())
         await state.set_state(st.CreateChar.name)
+    elif str(datetime.date(datetime.today())) == date:
+        await message.answer('Что-то ты зачастил со сменой персонажа. Давай завтра.', reply_markup=bt.ReplyKeyboardRemove())
     else:
         await message.answer('У тебя уже есть персонаж. Хочешь создать нового?', reply_markup=bt.kb_yesno)
         await state.set_state(st.CreateChar.choice)
@@ -30,19 +36,28 @@ async def new(message: Message, state: FSMContext):
 async def name(message: Message, state: FSMContext):
     user_data = await state.get_data()
     if message.text.lower() == 'да':
-        await message.answer('Как будут звать твоего нового персонажа?', reply_markup=bt.ReplyKeyboardRemove())
+        await message.answer('Как будут звать твоего нового персонажа?', reply_markup=bt.kb_back)
         await state.set_state(st.CreateChar.name)
     else:
         char = await user_data['user'].get_char_name()
         name = char.get('name')
-        await message.answer(f'Понимаю, сложно бросить своего мистера {name}', reply_markup=bt.ReplyKeyboardRemove())
+        await message.answer(f'Понимаю, сложно бросить такое чудо как {name}', reply_markup=bt.ReplyKeyboardRemove())
         await state.clear()
 
 
 @router.message(st.CreateChar.name)
 async def new_done(message: Message, state: FSMContext):
-    char = GeneratorPers(message.text, message.from_user.id)
-    await char.new()
-    await message.answer(f'Отныне звать его будут так. Чтобы посмотреть характеристики своего персонажа'
-                         f' введи команду /char', reply_markup=bt.ReplyKeyboardRemove())
-    await state.clear()
+    if message.text.lower() == 'назад':
+        user_data = await state.get_data()
+        char = await user_data['user'].get_char_name()
+        name = char.get('name')
+        await message.answer(f'Понимаю, сложно бросить такое чудо как {name}',
+                             reply_markup=bt.ReplyKeyboardRemove())
+        await state.clear()
+    else:
+        await state.set_state(st.CreateChar.choice)
+        char = GeneratorPers(message.text, message.from_user.id)
+        await char.new()
+        await message.answer(f'Отныне звать его будут так. Чтобы посмотреть характеристики своего персонажа'
+                             f' введи команду /char', reply_markup=bt.ReplyKeyboardRemove())
+        await state.clear()
