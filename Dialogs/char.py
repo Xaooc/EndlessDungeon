5192c9760c2855823183694e52fae544f9bc135a
Xaooc <101577885+C2Dimplementation@users.noreply.github.com>
2022-11-14 17:59:55 +0300

    Переделка id, чтобы персонажи для каждого чата бюыли новые

--- Dialogs/char.py
+++ Dialogs/char.py
@@ -11,20 +11,21 @@ router = Router()
 
 @router.message(Command("char"))
 async def char(message: Message, state: FSMContext):
-    user = UserData(tg_id=message.from_user.id)
+    tid = float(str(message.chat.id) + '.' + str(message.from_user.id))
+    user = UserData(tg_id=tid)
     #проверяем есть ли активный персонаж
     if user.is_user_inactive_char() or not user.is_user_created():
-        await message.answer('У тебя ещё нет активного персонажа. '
+        await message.reply('У тебя ещё нет активного персонажа. '
                              'Создать его ты можешь с помощью команды /new', reply_markup=bt.ReplyKeyboardRemove())
     else:
-        char = await user.get_char_name()
+        char = await user.get_char()
         name = char.get('name')
         con = char.get('con')
         dex = char.get('dex')
         mnd = char.get('mnd')
         gold = char.get('gold')
         hp = char.get('hp')
-        await message.answer(f'*Имя персонажа:* {name}  *Здоровье:* {hp}\n'
+        await message.reply(f'*Имя персонажа:* {name}  *Здоровье:* {hp}\n'
                              f'*Телосложение:* {con}  *Ловкость:* {dex}  *Мудрость:* {mnd}\n'
                              f'*Золото:* {gold}', parse_mode="MarkdownV2")
 
