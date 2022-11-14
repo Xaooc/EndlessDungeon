6bbb2afa841e3e2670c3c938c68f92b75127b51d
Xaooc <101577885+C2Dimplementation@users.noreply.github.com>
2022-11-14 18:02:00 +0300

    обновление формирование id и реплаи

--- Dialogs/new_char.py
+++ Dialogs/new_char.py
@@ -16,19 +16,20 @@ router = Router()
 @router.message(Command("new"))
 async def new(message: Message, state: FSMContext):
     #записываем экземпляр, чтобы передавать между статусами
-    user = UserData(tg_id=message.from_user.id)
+    tid = float(str(message.chat.id) + '.' + str(message.from_user.id))
+    user = UserData(tg_id=tid)
     await state.update_data(user=user)
-    date = await user.get_char_name()
-    date = date.get('date')
     if not user.is_user_created():
         await user.create_char()
+    date = await user.get_char()
+    date = date.get('date')
     if user.is_user_inactive_char():
-        await message.answer('Как зовут твоего персонажа?', reply_markup=bt.ReplyKeyboardRemove())
+        await message.reply('Как зовут твоего персонажа?', reply_markup=bt.ReplyKeyboardRemove())
         await state.set_state(st.CreateChar.name)
     elif str(datetime.date(datetime.today())) == date:
-        await message.answer('Что-то ты зачастил со сменой персонажа. Давай завтра.', reply_markup=bt.ReplyKeyboardRemove())
+        await message.reply('Что-то ты зачастил со сменой персонажа. Давай завтра.', reply_markup=bt.ReplyKeyboardRemove())
     else:
-        await message.answer('У тебя уже есть персонаж. Хочешь создать нового?', reply_markup=bt.kb_yesno)
+        await message.reply('У тебя уже есть персонаж. Хочешь создать нового?', reply_markup=bt.kb_yesno)
         await state.set_state(st.CreateChar.choice)
 
 
@@ -36,12 +37,12 @@ async def new(message: Message, state: FSMContext):
 async def name(message: Message, state: FSMContext):
     user_data = await state.get_data()
     if message.text.lower() == 'да':
-        await message.answer('Как будут звать твоего нового персонажа?', reply_markup=bt.kb_back)
+        await message.reply('Как будут звать твоего нового персонажа?', reply_markup=bt.kb_back)
         await state.set_state(st.CreateChar.name)
     else:
-        char = await user_data['user'].get_char_name()
+        char = await user_data['user'].get_char()
         name = char.get('name')
-        await message.answer(f'Понимаю, сложно бросить такое чудо как {name}', reply_markup=bt.ReplyKeyboardRemove())
+        await message.reply(f'Понимаю, сложно бросить такое чудо как {name}', reply_markup=bt.ReplyKeyboardRemove())
         await state.clear()
 
 
@@ -49,15 +50,16 @@ async def name(message: Message, state: FSMContext):
 async def new_done(message: Message, state: FSMContext):
     if message.text.lower() == 'назад':
         user_data = await state.get_data()
-        char = await user_data['user'].get_char_name()
+        char = await user_data['user'].get_char()
         name = char.get('name')
-        await message.answer(f'Понимаю, сложно бросить такое чудо как {name}',
+        await message.reply(f'Понимаю, сложно бросить такое чудо как {name}',
                              reply_markup=bt.ReplyKeyboardRemove())
         await state.clear()
     else:
         await state.set_state(st.CreateChar.choice)
-        char = GeneratorPers(message.text, message.from_user.id)
+        tid = float(str(message.chat.id) + '.' + str(message.from_user.id))
+        char = GeneratorPers(message.text, tid)
         await char.new()
-        await message.answer(f'Отныне звать его будут так. Чтобы посмотреть характеристики своего персонажа'
+        await message.reply(f'Отныне звать его будут так. Чтобы посмотреть характеристики своего персонажа'
                              f' введи команду /char', reply_markup=bt.ReplyKeyboardRemove())
         await state.clear()
--- Dialogs/start.py
+++ Dialogs/start.py
@@ -10,20 +10,21 @@ router = Router()
 
 @router.message(Command("start"))
 async def start(message: Message, state: FSMContext):
-    user = UserData(tg_id=message.from_user.id)
+    tid = float(str(message.chat.id) + '.' + str(message.from_user.id))
+    user = UserData(tg_id=tid)
     #проверяем есть ли юзер(на всякий случай)
     if user.is_user_created():
         #если есть, проверяем есть ли у него активный чар
         if user.is_user_inactive_char():
-            await message.answer('Привет! У тебя ещё нет персонажа. Создать его можно с помощью команды /new')
+            await message.reply('Привет! У тебя ещё нет персонажа. Создать его можно с помощью команды /new')
         else:
-            char = await user.get_char_name()
+            char = await user.get_char()
             name = char.get('name')
-            await message.answer(f'Привет! Твой {name} тебя заждался. '
+            await message.reply(f'Привет! Твой {name} тебя заждался. '
                                  f'Посмотреть его характеристики можно с помощью команды /char')
     else:
         #если юзера нет, то отправляем приветы
-        await message.answer('Добро пожаловать в бесконечные подземелья! '
+        await message.reply('Добро пожаловать в бесконечные подземелья! '
                              'У тебя ещё нет персонажа. Создать его можно с помощью команды /new \n'
                              'Подробнее об игре по команде /info')
         #добавляем юзера в бд
